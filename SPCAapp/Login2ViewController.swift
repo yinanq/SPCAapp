0e76ddf576c76cae6777283282831f9d821ffbf3
fargofremling <alicia.fremling@gmail.com>
2016-03-08 07:17:08 -0800

    update to login screens

--- SPCAapp/Login2ViewController.swift
+++ SPCAapp/Login2ViewController.swift
@@ -12,7 +12,9 @@ class Login2ViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signInActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
@@ -35,6 +37,38 @@ class Login2ViewController: UIViewController {
         
     }
 
+    @IBAction func didPressLoginButton(sender: AnyObject) {
+        if emailTextField.text!.isEmpty || passwordTextField.text!.isEmpty {
+            showAlertWithTitle("Email & Password Required", andMessage: "Please enter an email address and password.")
+        } else {
+            signInActivityIndicator.startAnimating()
+            delay(1) {
+                if self.emailTextField.text == "Email" && self.passwordTextField.text == "Password" {
+                    self.signInActivityIndicator.stopAnimating()
+                    self.performSegueWithIdentifier("signInButtonSegue", sender: self)
+                } else {
+                    self.signInActivityIndicator.stopAnimating()
+                    self.showAlertWithTitle("Invalid Email or Password", andMessage: "Please check the email and password combination.")
+                }
+            }
+        }
+    }
+    
+    func showAlertWithTitle(title: String, andMessage message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .Cancel){ (action) in }
+        alert.addAction(cancelAction)
+        presentViewController(alert, animated: true){}
+    }
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
     /*
     // MARK: - Navigation
 
