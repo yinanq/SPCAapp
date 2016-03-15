3d66790f409a227493f0afebfe565986e1e91cc4
Yinan <Yinan-Motiv@Yinans-Motiv-MacbookPro.local>
2016-03-14 20:14:24 -0700

    AnimalDetailsViewController work in progress

---
+++ AnimalDetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AnimalDetailsViewController.swift
+//  SPCAapp
+//
+//  Created by Yinan Motiv on 3/14/16.
+//  Copyright © 2016 AliciaAndYinan. All rights reserved.
+//
+
+import UIKit
+
+class AnimalDetailsViewController: UIViewController {
+
+    @IBOutlet weak var navItem: UINavigationItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navItem.title = "Dog!"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SPCAapp/MainListViewController.swift
+++ SPCAapp/MainListViewController.swift
@@ -62,14 +62,13 @@ class MainListViewController: UIViewController, UITableViewDataSource, UITableVi
         return cell
     }
     
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
+        //TODO:
+//        var destinationViewController.title = segue.destinationViewController as AnimalDetailsViewController
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
